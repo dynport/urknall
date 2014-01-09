2b5ce799f6dfcd03c3e873189315333b86a7a59e
Gereon Frey <gereon.frey@dynport.de>
2014-01-09 09:46:47 +0100

    don't import complete cmd namespace
    As the cmd package has a short name this is just not necessary.

--- host_packages.go
+++ host_packages.go
@@ -2,7 +2,7 @@ package urknall
 
 import (
 	"github.com/dynport/dgtk/goup"
-	. "github.com/dynport/urknall/cmd"
+	"github.com/dynport/urknall/cmd"
 )
 
 func newRunlist(name string, pkg Package, host *Host) *Runlist {
@@ -14,15 +14,15 @@ func (h *Host) buildSystemRunlists() {
 		h.addSystemPackage("hostname",
 			h.newHostPackage(
 				"hostname localhost", // Set hostname to make sudo happy.
-				&FileCommand{Path: "/etc/hostname", Content: h.Hostname},
-				&FileCommand{Path: "/etc/hosts", Content: "127.0.0.1 {{ .Hostname }} localhost"},
+				&cmd.FileCommand{Path: "/etc/hostname", Content: h.Hostname},
+				&cmd.FileCommand{Path: "/etc/hosts", Content: "127.0.0.1 {{ .Hostname }} localhost"},
 				"hostname -F /etc/hostname"))
 	}
 
 	if h.Timezone != "" {
 		h.addSystemPackage("timezone",
 			h.newHostPackage(
-				WriteFile("/etc/timezone", h.Timezone, "root", 0644),
+				cmd.WriteFile("/etc/timezone", h.Timezone, "root", 0644),
 				"dpkg-reconfigure --frontend noninteractive tzdata",
 			),
 		)
@@ -31,11 +31,11 @@ func (h *Host) buildSystemRunlists() {
 	if len(h.Firewall) > 0 {
 		h.addSystemPackage("firewall",
 			h.newHostPackage(
-				InstallPackages("iptables", "ipset"),
-				WriteAsset("/etc/network/if-pre-up.d/iptables", "fw_upstart.sh", "root", 0744),
-				WriteAsset("/etc/iptables/ipsets", "fw_ipset.conf", "root", 0644),
-				WriteAsset("/etc/iptables/rules_ipv4", "fw_rules_ipv4.conf", "root", 0644),
-				WriteAsset("/etc/iptables/rules_ipv6", "fw_rules_ipv6.conf", "root", 0644),
+				cmd.InstallPackages("iptables", "ipset"),
+				cmd.WriteAsset("/etc/network/if-pre-up.d/iptables", "fw_upstart.sh", "root", 0744),
+				cmd.WriteAsset("/etc/iptables/ipsets", "fw_ipset.conf", "root", 0644),
+				cmd.WriteAsset("/etc/iptables/rules_ipv4", "fw_rules_ipv4.conf", "root", 0644),
+				cmd.WriteAsset("/etc/iptables/rules_ipv6", "fw_rules_ipv6.conf", "root", 0644),
 				"{ modprobe iptable_filter && modprobe iptable_nat; }; /bin/true", // here to make sure next command succeeds.
 				"IFACE={{ .Interface }} /etc/network/if-pre-up.d/iptables"))
 	}
@@ -78,48 +78,48 @@ type dockerPackage struct {
 
 func (dp *dockerPackage) Package(rl *Runlist) {
 	rl.Add(
-		Or("grep universe /etc/apt/sources.list",
-			And("sed 's/main$/main universe/' -i /etc/apt/sources.list",
+		cmd.Or("grep universe /etc/apt/sources.list",
+			cmd.And("sed 's/main$/main universe/' -i /etc/apt/sources.list",
 				"DEBIAN_FRONTEND=noninteractive apt-get update")))
 	rl.Add(
-		InstallPackages("curl", "build-essential", "git-core", "bsdtar", "lxc", "aufs-tools"))
+		cmd.InstallPackages("curl", "build-essential", "git-core", "bsdtar", "lxc", "aufs-tools"))
 	rl.Add(
-		Or(
+		cmd.Or(
 			installDockerKernelOnRaring(),
 			installDockerKernelOnPrecise(),
 			"exit 1"))
 
 	rl.Add(dp.dockerBinary())
-	rl.Add(&UpstartCommand{Upstart: dp.createUpstart()})
+	rl.Add(&cmd.UpstartCommand{Upstart: dp.createUpstart()})
 	rl.Add("start docker")
 
 	if dp.Docker.WithRegistry {
-		rl.Add(WaitForUnixSocket("/var/run/docker.sock", 10))
+		rl.Add(cmd.WaitForUnixSocket("/var/run/docker.sock", 10))
 		rl.Add("docker run -d -p 0.0.0.0:5000:5000 stackbrew/registry")
 	}
 }
 
-func installDockerKernelOnRaring() *ShellCommand {
-	return And("lsb_release -c | grep raring",
-		InstallPackages("linux-image-extra-$(uname -r)"))
+func installDockerKernelOnRaring() *cmd.ShellCommand {
+	return cmd.And("lsb_release -c | grep raring",
+		cmd.InstallPackages("linux-image-extra-$(uname -r)"))
 }
 
-func installDockerKernelOnPrecise() *ShellCommand {
-	return And("lsb_release -c | grep precise",
-		IfNot("-f /etc/apt/sources.list.d/precise-updates.list",
-			And("echo 'deb http://archive.ubuntu.com/ubuntu precise-updates main' > /etc/apt/sources.list.d/precise-updates.list",
+func installDockerKernelOnPrecise() *cmd.ShellCommand {
+	return cmd.And("lsb_release -c | grep precise",
+		cmd.IfNot("-f /etc/apt/sources.list.d/precise-updates.list",
+			cmd.And("echo 'deb http://archive.ubuntu.com/ubuntu precise-updates main' > /etc/apt/sources.list.d/precise-updates.list",
 				"apt-get update -y")),
 		"apt-get -o Dpkg::Options::='--force-confnew' install linux-generic-lts-raring -y")
 }
 
-func (dp *dockerPackage) dockerBinary() *DownloadCommand {
+func (dp *dockerPackage) dockerBinary() *cmd.DownloadCommand {
 	baseUrl := "http://get.docker.io/builds/Linux/x86_64"
 
 	if dp.dockerVersion() < "0.6.0" {
 		panic("version lower than 0.6.0 not supported yet")
 	}
 	url := baseUrl + "/docker-" + dp.dockerVersion()
-	return DownloadToFile(url, "/usr/local/bin/docker", "root", 0700)
+	return cmd.DownloadToFile(url, "/usr/local/bin/docker", "root", 0700)
 }
 
 func (dp *dockerPackage) createUpstart() *goup.Upstart {
