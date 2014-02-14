eb2bb1e9d8c8d9cf9ba6192f6ebb456ba13bba91
Tobias Schwab <tobias.schwab@dynport.de>
2014-02-14 12:48:47 +0100

    fix exec call for docker

--- pkg/docker/package.go
+++ pkg/docker/package.go
@@ -35,7 +35,7 @@ start on runlevel [2345]
 stop on runlevel [!2345]
 {{ end }}
 
-exec /usr/local/bin/docker -d -D -H unix:///var/run/docker.sock -H tcp://{{ .NetworkInterface }}:4243 {{ with .DataDir }}-g={{ . }} 2>&1 | logger -i -t docker
+exec /usr/local/bin/docker -d -D -H unix:///var/run/docker.sock -H tcp://{{ .NetworkInterface }}:4243 {{ with .DataDir }}-g={{ . }}{{ end }} 2>&1 | logger -i -t docker
 `
 
 func (docker *Package) NetworkInterface() string {
