e63fb61d82522f98dde3a9cf10b51f625121200d
Gereon Frey <me@gereonfrey.de>
2014-02-28 15:14:30 +0100

    deleted old packages and commands, moved urknall binary

--- cmd/command_file_example_test.go
+++
@@ -1,8 +0,0 @@
-package cmd
-
-func ExampleWriteFile() {
-	// Create a file "/tmp/foo" with the content "Hello, World!". No owner and permissions will be set.
-	WriteFile("/tmp/foo", "Hello, World!", "", 0)
-	// Create a file "/tmp/bar" with the content "Hello, World!". Set owner to "gfrey" and permissions 0600.
-	WriteFile("/tmp/foo", "Hello, World!", "gfrey", 0600)
-}
