33b075c16a5d8479fe5cbc4a22411283c874a525
Gereon Frey <gereon.frey@dynport.de>
2014-03-04 15:11:43 +0100

    removed unused variable

--- constants.go
+++ constants.go
@@ -1,7 +1,5 @@
 package urknall
 
-var TimezoneUTC = "Etc/UTC"
-
 const (
 	ukGROUP    = "urknall"          // Group used for cache file ownership on host.
 	ukCACHEDIR = "/var/lib/urknall" // Directory for urknall cache.
