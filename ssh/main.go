c3bb5c1b67c97381e1f166b714f31c37984bfa24
Tobias Schwab <tobias.schwab@dynport.de>
2014-04-26 12:18:03 -0600

    only use agent auth if no password is set

--- ssh/main.go
+++ ssh/main.go
@@ -67,8 +67,7 @@ func (c *Host) Client() (*ssh.Client, error) {
 	}
 	if c.Password != "" {
 		config.Auth = append(config.Auth, ssh.Password(c.Password))
-	}
-	if sshSocket := os.Getenv("SSH_AUTH_SOCK"); sshSocket != "" {
+	} else if sshSocket := os.Getenv("SSH_AUTH_SOCK"); sshSocket != "" {
 		if c, e := net.Dial("unix", sshSocket); e == nil {
 			config.Auth = append(config.Auth, ssh.PublicKeysCallback(agent.NewClient(c).Signers))
 		}
