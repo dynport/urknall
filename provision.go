e2e777a18be533b2cdc648d941a52519197c6ee5
Tobias Schwab <tobias.schwab@dynport.de>
2014-04-26 10:43:56 -0600

    add provisioning type

--- provision.go
+++ provision.go
@@ -1,5 +1,28 @@
 package urknall
 
+type Provisioning struct {
+	Host   Host
+	List   *PackageList
+	DryRun bool
+}
+
+func (p *Provisioning) Run() error {
+	e := p.List.precompileRunlists()
+	if e != nil {
+		return e
+	}
+
+	runner := &Runner{
+		Host: p.Host,
+		User: p.Host.User(),
+	}
+	e = runner.prepare()
+	if e != nil {
+		return e
+	}
+	return runner.provision(p.List)
+}
+
 func Provision(host Host, list *PackageList) error {
 	return ProvisionWithOptions(host, list, nil)
 }
