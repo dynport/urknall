2a2d89f5b062f66d041c199737c7e4743e9c19a0
Tobias Schwab <tobias.schwab@dynport.de>
2014-04-26 12:26:37 -0600

    fix tests

--- host_test.go
+++ host_test.go
@@ -2,98 +2,44 @@ package urknall
 
 import (
 	"testing"
+
+	"github.com/dynport/urknall/ssh"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestUserHandling(t *testing.T) {
 	Convey("Given a host with no user specified", t, func() {
-		user := ""
 		Convey("When the New function is called", func() {
-			h := &Host{User: user}
+			h := &ssh.Host{Address: "127.0.0.1"}
 			Convey("Then a host value is returned", func() {
 				So(h, ShouldNotBeNil)
 			})
 			Convey("The returned host's user equals root", func() {
-				So(h.user(), ShouldEqual, "root")
-			})
-			Convey("The returned host's 'isSudoRequired' predicate return false", func() {
-				So(h.isSudoRequired(), ShouldBeFalse)
+				So(h.User(), ShouldEqual, "root")
 			})
 		})
 	})
 
 	Convey("Given a host with user set to 'root'", t, func() {
-		user := "root"
 		Convey("When the New function is called", func() {
-			h := &Host{User: user}
+			h := &ssh.Host{Address: "root@127.0.0.1"}
 			Convey("Then a host value is returned", func() {
 				So(h, ShouldNotBeNil)
 			})
 			Convey("The returned host's user equals root", func() {
-				So(h.user(), ShouldEqual, "root")
-			})
-			Convey("The returned host's 'isSudoRequired' predicate return false", func() {
-				So(h.isSudoRequired(), ShouldBeFalse)
+				So(h.User(), ShouldEqual, "root")
 			})
 		})
 	})
 
 	Convey("Given a host with user set to something not being 'root'", t, func() {
-		user := "gfrey"
 		Convey("When the New function is called", func() {
-			h := &Host{User: user}
+			h := &ssh.Host{Address: "gfrey@127.0.0.1"}
 			Convey("Then a host value is returned", func() {
 				So(h, ShouldNotBeNil)
 			})
 			Convey("The returned host's user equals the given one", func() {
-				So(h.user(), ShouldEqual, user)
-			})
-			Convey("The returned host's 'isSudoRequired' predicate return true", func() {
-				So(h.isSudoRequired(), ShouldBeTrue)
-			})
-		})
-	})
-}
-
-func TestInterfaceHandling(t *testing.T) {
-	defaultInterface := "eth0"
-	Convey("Given a host", t, func() {
-		h := &Host{IP: "127.0.0.1"}
-		list := &PackageList{}
-		Convey("When no interface is set", func() {
-			Convey("And the Interface method is called", func() {
-				v := h.publicInterface()
-				Convey("Then the interface is set to the default", func() {
-					So(v, ShouldEqual, defaultInterface)
-				})
-			})
-		})
-
-		Convey("When adding packages", func() {
-			So(func() { list.AddPackage("pkg", nil) }, ShouldNotPanic)
-			So(func() { list.AddPackage("other_pkg", nil) }, ShouldNotPanic)
-			Convey("when adding a package with the same name", func() {
-				So(func() { list.AddPackage("pkg", nil) }, ShouldPanic)
-			})
-		})
-
-		Convey("When the interface is explicitly set to the default", func() {
-			h.Interface = defaultInterface
-			Convey("And the Interface method is called", func() {
-				v := h.publicInterface()
-				Convey("Then the interface is set to the default", func() {
-					So(v, ShouldEqual, defaultInterface)
-				})
-			})
-		})
-
-		Convey("When the interface is set to 'tun0'", func() {
-			h.Interface = "tun0"
-			Convey("And the Interface method is called", func() {
-				v := h.publicInterface()
-				Convey("Then the interface is set to 'tun0'", func() {
-					So(v, ShouldEqual, "tun0")
-				})
+				So(h.User(), ShouldEqual, "gfrey")
 			})
 		})
 	})
