9e3f6e432e04263e10d79a4890fb034c2c0b7d62
Gereon Frey <me@gereonfrey.de>
2014-05-06 15:44:46 +0200

    moved build method to other file

--- urknall.go
+++ urknall.go
@@ -44,3 +44,17 @@ func OpenStdoutLogger() io.Closer {
 func OpenSimpleStdoutLogger() io.Closer {
 	return pubsub.OpenStdoutLoggerWithFormatter(pubsub.SimpleFormatter)
 }
+
+func Build(tgt Target, pkg *Package) error {
+	e := pkg.precompileRunlists()
+	if e != nil {
+		return e
+	}
+
+	runner := &Runner{Target: tgt}
+	e = runner.prepare()
+	if e != nil {
+		return e
+	}
+	return runner.provision(pkg)
+}
