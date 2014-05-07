9d6c97e4165b2e8cec0622993f7221fc39f5bb4c
Gereon Frey <me@gereonfrey.de>
2014-05-07 16:46:47 +0200

    moved rawCommand to command.go file

--- command.go
+++ command.go
@@ -1,6 +1,26 @@
 package urknall
 
-import "github.com/dynport/urknall/utils"
+import (
+	"fmt"
+
+	"github.com/dynport/urknall/cmd"
+	"github.com/dynport/urknall/utils"
+)
+
+type rawCommand struct {
+	cmd.Command        // The command to be executed.
+	checksum    string // The checksum of the command.
+	task        *Task
+}
+
+func (cmd *rawCommand) execute(build *Build, checksumDir string) (e error) {
+	sCmd := fmt.Sprintf("sh -x -e -c %q", cmd.Shell())
+	for _, env := range build.Env {
+		sCmd = env + " " + sCmd
+	}
+	r := &remoteTaskRunner{build: build, cmd: sCmd, rawCommand: cmd, dir: checksumDir}
+	return r.run()
+}
 
 type stringCommand struct {
 	cmd string
