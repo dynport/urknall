0b51f80e52e300ab7fbd4a693efd95ea4db70662
Tobias Schwab <tobias.schwab@dynport.de>
2014-07-15 23:26:36 +0200

    add example main file

--- examples/main.go
+++ examples/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/dynport/urknall"
+)
+
+var logger = log.New(os.Stderr, "", 0)
+
 func main() {
-	// intentionally left blank
+	if e := run(); e != nil {
+		logger.Fatal(e)
+	}
+}
+
+type Template struct {
+}
+
+func (tpl *Template) Render(p urknall.Package) {
+	p.AddCommands("hello", Shell("echo hello world"))
+}
+
+func run() error {
+	defer urknall.OpenLogger(os.Stdout).Close()
+	var target urknall.Target
+	var e error
+	uri := "ubuntu@my.host"
+	password := ""
+	if password != "" {
+		target, e = urknall.NewSshTargetWithPassword(uri, password)
+	} else {
+		target, e = urknall.NewSshTarget(uri)
+	}
+	if e != nil {
+		return e
+	}
+	return urknall.Run(target, &Template{})
 }
