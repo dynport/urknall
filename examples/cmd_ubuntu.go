5d3cb852d6b9717e01e2dffdcf3e76dd293b30c4
Gereon Frey <gereon.frey@dynport.de>
2014-07-30 09:09:17 +0200

    added support for apt pkg update for selected repos

--- examples/cmd_ubuntu.go
+++ examples/cmd_ubuntu.go
@@ -10,6 +10,20 @@ func UpdatePackages() *ShellCommand {
 	return And("apt-get update", "DEBIAN_FRONTEND=noninteractive apt-get upgrade -y")
 }
 
+// Update the package cache for a given repository only. Repo selection is done
+// via the name of apt's configuration file taken from /etc/apt/sources.list.d.
+// This is much faster if you just added a repo and want to install software as
+// you need not update all other packages too (which most probably happened
+// just recently during provisioning).
+func UpdateSelectedRepoPackages(repoConfigPath string) *ShellCommand {
+	return &ShellCommand{
+		Command: fmt.Sprintf(
+			`apt-get update -o Dir::Etc::sourcelist="sources.list.d/%s" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"`,
+			repoConfigPath,
+		),
+	}
+}
+
 // Install the given packages using apt-get. At least one package must be given (pkgs can be left empty).
 func InstallPackages(pkg string, pkgs ...string) *ShellCommand {
 	return &ShellCommand{
