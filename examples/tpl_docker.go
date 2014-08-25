76341798e1211e9cc3c2d6efc2e4220533ef4bc5
Tobias Schwab <tobias.schwab@dynport.de>
2014-08-25 15:42:16 +0200

    allow custom install dirs, allow binding to public interface, fix default port

--- examples/tpl_docker.go
+++ examples/tpl_docker.go
@@ -3,7 +3,9 @@ package main
 import "github.com/dynport/urknall"
 
 type Docker struct {
-	Version string `urknall:"required=true"` // e.g. 1.1.0
+	Version          string `urknall:"required=true"` // e.g. 1.1.0
+	CustomInstallDir string
+	Public           bool
 }
 
 func (tpl *Docker) Render(p urknall.Package) {
@@ -15,12 +17,15 @@ func (tpl *Docker) Render(p urknall.Package) {
 	p.AddCommands("upstart", WriteFile("/etc/init/docker.conf", dockerUpstart, "root", 0644))
 }
 
-const dockerUpstart = `exec {{ .InstallDir }}/bin/docker -d -H tcp://127.0.0.1:5432 -H unix:///var/run/docker.sock 2>&1 | logger -i -t docker
+const dockerUpstart = `exec {{ .InstallDir }}/bin/docker -d -H tcp://{{ if .Public }}0.0.0.0{{ else }}127.0.0.1{{ end }}:4243 -H unix:///var/run/docker.sock 2>&1 | logger -i -t docker
 `
 
 func (tpl *Docker) InstallDir() string {
 	if tpl.Version == "" {
 		panic("Version must be set")
+	}
+	if tpl.CustomInstallDir != "" {
+		return tpl.CustomInstallDir
 	}
 	return "/opt/docker-" + tpl.Version
 }
